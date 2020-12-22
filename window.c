f154dd41ba07a6f123f2f7415bd9f9f06135fa67
Mikhail <45981435+mihan-pro@users.noreply.github.com>
2020-12-22 23:50:11 +0300

    solved problems with compiling

--- window.c
+++ window.c
@@ -3,18 +3,25 @@
 #include "window.h"
 #include "base.h"
 
-struct window_t *window_create(char *name, int startX, int startY, int endX, int endY, char attribute, int typeOfBorder)
+struct window_t *window_create(char *window_name, int startX, int startY, int endX, int endY, char attribute, int typeOfBorder)
 {
-    struct window_t window = {name, startX, startY, endX, endY, attribute, typeOfBorder};
-    return &window;
+    struct window_t *window = malloc(sizeof(struct window_t));
+    window->name = window_name;
+    window->startX = startX;
+    window->startY = startY;
+    window->endX = endX;
+    window->endY = endY;
+    window->attribute = attribute;
+    window->typeOfBorder = typeOfBorder;
+    return window;
 }
 
-void window_open(struct window_t *window, char* buffer)
+void window_open(struct window_t *window, char *buffer)
 {
     takeVideoMemory(buffer);
     particularDescktop(window->startX, window->startY, window->endX, window->endY, window->attribute, window->name, window->typeOfBorder);
 }
-void window_close(char* buffer)
+void window_close(char *buffer)
 {
     putVideoMemory(buffer);
 }
